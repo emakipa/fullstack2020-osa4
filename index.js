4f31d646bd6a8c51803a1ed2fcf249d483f7fdf2
Esa Mäkipää <esa.makipaa@gmail.com>
2020-09-07 14:40:33 +0300

    models folder created with blog.js

--- index.js
+++ index.js
@@ -4,20 +4,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
 //returns the changed object to caller
 mongoose.set('useFindAndModify', false)
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -62,15 +54,6 @@ app.post('/api/blogs', (request, response, next) => {
       response.status(201).json(savedAndFormattedBlog)
     })
     .catch(error => next(error))
-})
-
-//edit the object returned by Mongoose
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
 })
 
 // unknown endpoint handling

249265325093115551fe831459806ff8f0e62009
Esa Mäkipää <esa.makipaa@gmail.com>
2020-09-07 15:22:57 +0300

    app.js created and index.js updated accordingly

---
+++ app.js
@@ -0,0 +1,36 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+//returns the changed object to caller
+mongoose.set('useFindAndModify', false)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+//custom request logger
+app.use(middleware.requestLogger)
+
+//routes
+app.use('/api/blogs', blogsRouter)
+
+// unknown endpoint handling
+app.use(middleware.unknownEndpoint)
+
+// unknown request handling
+app.use(middleware.errorHandler)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
 
-//returns the changed object to caller
-mongoose.set('useFindAndModify', false)
+const server = http.createServer(app)
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
-app.use(cors())
-app.use(express.json())
-
-//custom request logger
-app.use(middleware.requestLogger)
-
-//routes
-app.use('/api/blogs', blogsRouter)
-
-// unknown endpoint handling
-app.use(middleware.unknownEndpoint)
-
-// unknown request handling
-app.use(middleware.errorHandler)
-
-//const PORT = process.env.PORT || 3003
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })

e854c822bfbf1caf48695dc90d8360b1050199a4
Esa Mäkipää <esa.makipaa@gmail.com>
2020-09-07 12:43:08 +0300

    Comments removed from index.js, create_blog.rest updated

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-//first line
-//console.log('Hello World!')
-
-//exercise 4.1 draft, version for editing
-//1. express installed
-//2. cors installed
-//3. mongoose installed
-//4. database connection tested with separate mongo.js file
-//5. nodemon installed for development dependency and added script
-//   "dev": "nodemon index.js", package.json-tiedostoon
-//6. application draft now starts teh application: npm run dev (errors exist)
-//7. const http = require('http') commented, nort used
-//8. dotenv installed
-//9. created .env file for MONGOBD_URI and PORT
-//10. process.env taken into use
-//11. 'useFindAndModify' set to false (returns the changed object to caller)
-//12. application starts and runs and can get blogs saved in the database
-//13. added editing object returned by Mongoose
-//14. requestLogger taken into use
-//15. unknownEndpoint and errorHandler taken into use
-
-
-//const http = require('http')
 require('dotenv').config()
+const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -126,50 +104,3 @@ const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-//original exercise 4.1 draft
-/*
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-*/
